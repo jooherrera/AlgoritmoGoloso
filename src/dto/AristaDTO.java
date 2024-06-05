fa199b590cd3ff8574a0f47be994d2a67c428c9f
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-06-05 12:24:55 -0300

    feat: clase AristaDTO

--- src/dto/AristaDTO.java
+++ src/dto/AristaDTO.java
@@ -1,5 +1,20 @@
 package dto;
 
 public class AristaDTO {
+	
+	private int _vertice1;
+	private int _vertice2;
+	
+	public AristaDTO(int _vertice1, int _vertice2) {
+		this._vertice1 = _vertice1;
+		this._vertice2 = _vertice2;
+	}
+
+	public int get_vertice1() {
+		return _vertice1;
+	}
+	public int get_vertice2() {
+		return _vertice2;
+	}
 
 }
