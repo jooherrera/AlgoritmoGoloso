bcf95321a08052104ec656ae47b67f60c5b46377
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-06-05 12:44:10 -0300

    feat: clase VerticeDTO

--- src/dto/VerticeDTO.java
+++ src/dto/VerticeDTO.java
@@ -1,5 +1,30 @@
 package dto;
 
 public class VerticeDTO {
-
+	
+	private int _id;
+	private double _peso;
+	private double _latitud;
+	private double _longitud;
+		
+	public VerticeDTO(int id, double peso, double latitud, double longitud) {
+		_id = id;
+		_peso = peso;
+		_latitud = latitud;
+		_longitud = longitud;
+	}
+	
+	public int get_id() {
+		return _id;
+	}
+	public double get_peso() {
+		return _peso;
+	}
+	public double get_latitud() {
+		return _latitud;
+	}
+	public double get_longitud() {
+		return _longitud;
+	}
+	
 }
